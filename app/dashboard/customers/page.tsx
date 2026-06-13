79e7127d66760391ef1bd158ea11e3c14183c513
Mohamad javad samadi <javadsamadi255@gmail.com>
2026-06-13 21:49:57 +0330

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      customers page
+    </div>
+  )
+}
+
+export default page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      invoice page
+    </div>
+  )
+}
+
+export default page
