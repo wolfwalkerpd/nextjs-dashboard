0715514ccdaad447f06b5da6430e9b742b60974c
Mohamad javad samadi <javadsamadi255@gmail.com>
2026-06-21 14:52:23 +0330

    third commit

---
+++ .pnpmfile.cjs
@@ -0,0 +1,3 @@
+module.exports = {
+  onlyBuiltDependencies: ["bcrypt", "sharp"],
+};
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,18 +9,18 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const handleSearch =useDebouncedCallback((term: string)=> {
+  const handleSearch = useDebouncedCallback((term: string)=> {
     console.log(`searching... ${term}`)
 
     const params = new URLSearchParams(searchParams);
-
+    params.set('page', 1)
     if (term) {
       params.set("query", term);
     } else {
       params.delete("query");
     }
     replace(`${pathname}?${params.toString()}`)
-  })
+  },300)
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
